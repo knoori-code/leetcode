c1cafe00aafcb04e0347a05551de37c966619c34
knoori-code <knoori3@gmail.com>
2023-04-12 17:24:19 -0400

    Improve two sum code

--- 1-twoSum.js
+++ 1-twoSum.js
@@ -1,9 +1,24 @@
-var twoSum = function(nums, target) {
+// var twoSum = function(nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//       for (let j = 0; j < nums.length; j++) {
+//           if (nums[i] + nums[j] === target && i !== j) {
+//               return [i, j]
+//           }
+//       }
+//   }
+// };
+
+const twoSum = function (nums, target) {
+  const obj = {};
+  let complement;
+
   for (let i = 0; i < nums.length; i++) {
-      for (let j = 0; j < nums.length; j++) {
-          if (nums[i] + nums[j] === target && i !== j) {
-              return [i, j]
-          }
-      }
+    complement = target - nums[i];
+
+    if (obj[complement] !== undefined && obj[complement] !== i) {
+      return [obj[complement], i];
+    }
+
+    obj[nums[i]] = i;
   }
-}; 
+};
