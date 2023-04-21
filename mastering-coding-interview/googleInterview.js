87105ffc40e8c96c6876d115238f63f48d52ccaf
knoori-code <knoori3@gmail.com>
2023-04-20 21:01:54 -0400

    Solve using more efficient solution

--- mastering-coding-interview/googleInterview.js
+++ mastering-coding-interview/googleInterview.js
@@ -1,9 +1,32 @@
-const array = [6,4,3,2,1,7];
-const target = 9;
+const array = [6, 4, 3, 2, 1, 7];
+const sum = 9;
 
 // Brute force method
 function sumInArray(arr, tar) {
-  for (let i = 0; i < arr.length; i++) {
-    
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === tar) {
+        return true;
+      }
+    }
   }
+
+  return false;
 }
+
+
+const sumInArrayImproved = (arr, sum) => {
+  const set = new Set();
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (set.has(arr[i])) {
+      return true;
+    }
+    
+    set.add(sum - arr[i]);
+  }
+  
+  return false
+}
+
+console.log(sumInArrayImproved([6, 4, 3, 2, 1, 7], 10));
