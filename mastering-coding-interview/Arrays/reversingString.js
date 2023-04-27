f6d0924f635b7adfa8bcb8af528a4239aa6aedf3
knoori-code <knoori3@gmail.com>
2023-04-27 18:37:49 -0400

    Solve using input validation

--- mastering-coding-interview/Arrays/reversingString.js
+++ mastering-coding-interview/Arrays/reversingString.js
@@ -1,9 +1,13 @@
 function reverse(str){
-  const array = str.split("");
+  // Check input
+  if (typeof str !== "string" || !str || str.length < 2) {
+    return "Please enter a valid string"
+  }
+
   const reversedArray = [];
   
-  for (let i = array.length - 1; i >= 0; i--) {
-      reversedArray.push(array[i]);
+  for (let i = str.length - 1; i >= 0; i--) {
+      reversedArray.push(str[i]);
   }
   return reversedArray.join("")
 }
