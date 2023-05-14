b9efd6547ec5b677a4d4cbb5f9131302f3123779
knoori-code <knoori3@gmail.com>
2023-05-13 22:58:23 -0400

    Alternate solution for moving zeroes to end

--- arrays/283-moveZeroes.js
+++ arrays/283-moveZeroes.js
@@ -1,11 +1,26 @@
 var moveZeroes = function(nums) {
-  let insertPos = 0;
+  // let insertPos = 0;
+  // for (let i = 0; i < nums.length; i++) {
+  //     if (nums[i] !== 0) {
+  //         nums[insertPos++] = nums[i];
+  //     }
+  // }
+  // while (insertPos < nums.length) {
+  //     nums[insertPos++] = 0;
+  // }
+
+  // Alternate solution
+  let leftPointer = 0;
+
+  // i is the right pointer
   for (let i = 0; i < nums.length; i++) {
-      if (nums[i] !== 0) {
-          nums[insertPos++] = nums[i];
-      }
+    if (nums[i] !== 0) {
+      let leftCopy = nums[leftPointer];
+      nums[leftPointer] = nums[i];
+      nums[i] = leftCopy;
+      leftPointer++
+    }
   }
-  while (insertPos < nums.length) {
-      nums[insertPos++] = 0;
-  }
+
+  return nums;
 };
