f25ee26c138877f487b90299ec17c0911bd9ae8b
knoori-code <knoori3@gmail.com>
2023-05-17 23:29:08 -0400

    Solve using efficient solution

--- arrays/49-groupAnagrams.js
+++ arrays/49-groupAnagrams.js
@@ -1,19 +1,14 @@
-var groupAnagrams = function(strs) {
+var groupAnagrams = function (strs) {
   const obj = {};
-  const array = [];
 
   for (let word of strs) {
-      const sortedWord = word.split('').sort().join('')
-      if (!obj[sortedWord]) {
-          obj[sortedWord] = [word];
-      } else {
-          obj[sortedWord].push(word);
-      }
+    const sortedWord = word.split("").sort().join("");
+    if (!obj[sortedWord]) {
+      obj[sortedWord] = [word];
+    } else {
+      obj[sortedWord].push(word);
+    }
   }
 
-  for (let key in obj) {
-      array.push(obj[key]);
-  }
-
-  return array;
+  return Object.values(obj);
 };
