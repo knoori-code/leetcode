78f31089d6f52afad72bd9ad6bb24d51d19f0712
knoori-code <knoori3@gmail.com>
2023-05-18 23:54:41 -0400

    Solve sum of unique elements

---
+++ arrays/1748-sumUniqueElements.js
@@ -0,0 +1,20 @@
+var sumOfUnique = function(nums) {
+  const obj = {};
+  let sum = 0;
+
+  for (let num of nums) {
+      if (!obj[num]) {
+          obj[num] = 0;
+      }
+      obj[num]++;
+  }
+
+
+  for (let key in obj) {
+      if (obj[key] === 1) {
+          sum += Number(key);
+      }
+  }
+
+  return sum;
+};
